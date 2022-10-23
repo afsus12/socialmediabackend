0b869a122f293011cff4a773f6efd2e1c154c7bf
afsus12 <afsayassine@gmail.com>
2022-10-24 00:58:15 +0100

    nkn

--- server.js
+++ server.js
@@ -1,7 +1,51 @@
-var express = require('express'),
-    app = express(),
-    port = process.env.PORT || 4000;
+const bodyParser = require('body-parser');
+const { response } = require('express');
+const express = require('express');
+const    app = express();
+const   port = process.env.PORT || 4000;
+const mongoose=require('mongoose');
+const User=require("./models/users")
+mongoose.connect('mongodb://localhost:27017/socialmedia',
+{
+ useNewUrlParser:true,
+ useUnifiedTopology:true
+
+})
+app.use(bodyParser.json());
+app.get("/",async (req,res )=> {
+
+res.send("hello");
+
+} );
+app.post("/",async (req,res )=> {
+
+    res.send("hello");
+    
+    } );
+    app.post("/api/register",async (req,res )=> {
  
+        console.log(req.body)
+        const {username,password}=req.body
+      /*   const password=await bcrypt.hash(password,10) */
+         res.json({status:'ok'+req.body["username"]})
+    try{
+       const response =await User.create(
+              { username,password
+              }
+              )
+
+console.log(response)
+
+
+    } catch (error){
+        console.log(error)
+        return res.json({status:"erorr"})
+    }
+
+
+        } );
+
+
 app.listen(port, () => {
     console.log('Server started on: ' + port);
 });
